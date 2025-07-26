df169034b5c9b9a96b37dc2668c17804dd009faf
Tshimangadzo <tshimangadzostephen@gmail.com>
2025-07-26 11:23:38 +0200

    Update Services.tsx

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -8,8 +8,8 @@ const Services = () => {
 
   return (
     <section className="services section" id="services">
-      <h2 className="section__title">Services</h2>
-      <span className="section__subtitle">Services Offered</span>
+      <h2 className="section__title">What I Offer</h2>
+      <span className="section__subtitle">Highlights</span>
       <div className="services__container container grid">
         <div className="services__content web_design_content">
           <div>
