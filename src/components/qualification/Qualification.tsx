dfdb148ce85bbfe90f0e151041d9604fe71a3fb8
Tshimangadzo <tshimangadzostephen@gmail.com>
2025-12-31 07:37:02 +0200

    revert to old timeline and change tabs only

--- src/components/qualification/Qualification.tsx
+++ src/components/qualification/Qualification.tsx
@@ -5,10 +5,7 @@ import moyo_img from "../../assets/moyo-logo.png";
 import rev_img from "../../assets/rev logo.webp";
 
 const Qualification = () => {
-  const [toggleState, setToggleState] = useState(1);
-  const toggleTab = (index: any) => {
-    setToggleState(index);
-  };
+  const [toggleState, setToggleState] = useState<number>(1);
 
   return (
     <section className="qualification section">
@@ -16,47 +13,42 @@ const Qualification = () => {
       <span className="section__subtitle">Academic & Professional Journey</span>
 
       <div className="qualification__container container">
+        {/* Tabs */}
         <div className="qualification__tabs">
           <div
-            className={
-              toggleState === 1
-                ? "qualification__button button--flex  qualification__active"
-                : "qualification__button button--flex"
-            }
-            onClick={() => toggleTab(1)}
+            className={`qualification__button button--flex ${
+              toggleState === 1 ? "qualification__active" : ""
+            }`}
+            onClick={() => setToggleState(1)}
           >
-            {/* <i className="uil uil-graduation-cap qualification-icon"></i> */}
             Education
           </div>
-
           <div
-            className={
-              toggleState === 2
-                ? "qualification__button button--flex  qualification__active"
-                : "qualification__button button--flex"
-            }
-            onClick={() => toggleTab(2)}
+            className={`qualification__button button--flex ${
+              toggleState === 2 ? "qualification__active" : ""
+            }`}
+            onClick={() => setToggleState(2)}
           >
-            {/* <i className="uil uil-briefcase-alt qualification-icon"></i> */}
             Work History
           </div>
         </div>
 
+        {/* Sections */}
         <div className="qualification__sections">
+          {/* Education */}
           <div
-            className={
-              toggleState === 1
-                ? "qualification__content qualification__content-active"
-                : "qualification__content"
-            }
+            className={`qualification__content ${
+              toggleState === 1 ? "qualification__content-active" : ""
+            }`}
           >
+            {/* First education item */}
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">
-                  Informatics -Information Systems
+                  Informatics - Information Systems
                 </h3>
                 <span className="qualification__subtitle">
-                  <img src={Up_Img} alt="image" className="up__img" />
+                  <img src={Up_Img} alt="University" className="up__img" />
                   University of Pretoria
                 </span>
                 <div className="qualification__calendar">
@@ -69,19 +61,18 @@ const Qualification = () => {
               </div>
             </div>
 
+            {/* Second education item */}
             <div className="qualification__data">
-              <div></div>
+              <div></div> {/* Empty left side */}
               <div>
                 <span className="qualification__rounder"></span>
-                {/* <span className="qualification__line"></span> */}
               </div>
-
               <div>
                 <h3 className="qualification__title">
                   Information and Knowledge Systems - Genetics
                 </h3>
                 <span className="qualification__subtitle">
-                  <img src={Up_Img} alt="image" className="up__img" />
+                  <img src={Up_Img} alt="University" className="up__img" />
                   University of Pretoria
                 </span>
                 <div className="qualification__calendar">
@@ -91,20 +82,18 @@ const Qualification = () => {
             </div>
           </div>
 
+          {/* Work */}
           <div
-            className={
-              toggleState === 2
-                ? "qualification__content qualification__content-active"
-                : "qualification__content"
-            }
+            className={`qualification__content ${
+              toggleState === 2 ? "qualification__content-active" : ""
+            }`}
           >
+            {/* First work item */}
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">
-                  Software Developer
-                </h3>
+                <h3 className="qualification__title">Software Developer</h3>
                 <span className="qualification__subtitle">
-                  <img src={moyo_img} alt="image" className="moyo__img" />
+                  <img src={moyo_img} alt="Moyo" className="moyo__img" />
                   Moyo Business Advisory
                 </span>
                 <div className="qualification__calendar">
@@ -117,41 +106,23 @@ const Qualification = () => {
               </div>
             </div>
 
+            {/* Second work item */}
             <div className="qualification__data">
-              <div></div>
+              <div></div> {/* Empty left side */}
               <div>
                 <span className="qualification__rounder"></span>
-                {/* <span className="qualification__line"></span> */}
               </div>
-
               <div>
                 <h3 className="qualification__title">Transcriptionist</h3>
                 <span className="qualification__subtitle">
-                  <img src={rev_img} alt="image" className="rev_img" />
+                  <img src={rev_img} alt="Rev" className="rev_img" />
                   Rev
                 </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i>2020-2022
+                  <i className="uil uil-calendar-alt"></i>2020 - 2022
                 </div>
               </div>
             </div>
-
-            {/* <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Transcriptionist</h3>
-                <span className="qualification__subtitle">
-                  <img src={rev_img} alt="image" className="rev_img" />
-                  Rev
-                </span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i>2020-2022
-                </div>
-              </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                 <span className="qualification__line"></span>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
